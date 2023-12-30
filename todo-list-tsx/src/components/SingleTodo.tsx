e9a5da2379a1a9b78c6890dd7591e90761487a7d
Adenike23 <adeofeadenike@gmail.com>
2023-12-30 16:10:57 +0100

    implemented drag feature

--- todo-list-tsx/src/components/SingleTodo.tsx
+++ todo-list-tsx/src/components/SingleTodo.tsx
@@ -3,14 +3,16 @@ import { Todo } from "./model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import "./styles.css";
+import { Draggable } from "react-beautiful-dnd";
 
 type Props = {
   t: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  index: number;
 };
 
-const SingleTodo = ({ t, todos, setTodos }: Props) => {
+const SingleTodo = ({ t, todos, setTodos, index }: Props) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(t.todo);
 
@@ -24,51 +26,60 @@ const SingleTodo = ({ t, todos, setTodos }: Props) => {
   const handleEdit = (e: React.FormEvent, id: number) => {
     e.preventDefault();
 
-    setTodos(todos.map((t) => (t.id === id ? { ...t, todo:editTodo } : t)));
+    setTodos(todos.map((t) => (t.id === id ? { ...t, todo: editTodo } : t)));
     setEdit(false);
   };
   //whenever the 'edit' changes, this useEffect fires of
   useEffect(() => {
     inputRef.current?.focus();
-  }, [edit])
+  }, [edit]);
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="todos__single" onSubmit={(e) => handleEdit(e, t.id)}>
-      {edit ? (
-        <input
-          value={editTodo}
-          onChange={(e) => setEditTodo(e.target.value)}
-          className="todos__single--text"
-          ref={inputRef}
-          
-        />
-      ) : t.isDone ? (
-        <s className="todos__single--text">{t.todo}</s>
-      ) : (
-        <span className="todos__single--text">{t.todo}</span>
-      )}
-
-      <div>
-        <span
-          className="icon"
-          onClick={() => {
-            //if edit is not on & isDone is not done, then setEdit to close
-            if (!edit && !t.isDone) {
-              setEdit(!edit);
-            }
-          }}
+    <Draggable draggableId={t.id.toString()} index={index}>
+      {(provided, snapshot) => (
+        <form
+          className={`todos__single ${snapshot.isDragging? 'drag' : ''}`}
+          onSubmit={(e) => handleEdit(e, t.id)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <AiFillEdit />
-        </span>
-        <span className="icon" onClick={() => handleDelete(t.id)}>
-          <AiFillDelete />
-        </span>
-        <span className="icon" onClick={() => handleDone(t.id)}>
-          <MdDone />
-        </span>
-      </div>
-    </form>
+          {edit ? (
+            <input
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className="todos__single--text"
+              ref={inputRef}
+            />
+          ) : t.isDone ? (
+            <s className="todos__single--text">{t.todo}</s>
+          ) : (
+            <span className="todos__single--text">{t.todo}</span>
+          )}
+
+          <div>
+            <span
+              className="icon"
+              onClick={() => {
+                //if edit is not on & isDone is not done, then setEdit to close
+                if (!edit && !t.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <AiFillEdit />
+            </span>
+            <span className="icon" onClick={() => handleDelete(t.id)}>
+              <AiFillDelete />
+            </span>
+            <span className="icon" onClick={() => handleDone(t.id)}>
+              <MdDone />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
   );
 };
 
